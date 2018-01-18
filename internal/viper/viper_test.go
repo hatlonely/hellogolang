8eb1c8eaf8e392455210de155223e997f3517a92
hatlonely <hatlonely@gmail.com>
2018-01-18 12:25:07 +0800

    update

--- internal/viper/viper_test.go
+++ internal/viper/viper_test.go
@@ -3,11 +3,50 @@ package viper
 import (
 	"testing"
 	. "github.com/smartystreets/goconvey/convey"
+	"io/ioutil"
+	"github.com/spf13/viper"
+	"os"
 )
 
 func TestViper(t *testing.T) {
 	Convey("Given 给一个配置文件", t, func() {
+		filename := "test.json"
+		context := `{
+    "host": {
+        "address": "localhost",
+        "port": 5799
+    },
+    "datastore": {
+        "metric": {
+            "host": "127.0.0.1",
+            "port": 3099
+        },
+        "warehouse": {
+            "host": "198.0.0.1",
+            "port": 2112
+        }
+    }
+}`
 
+		err := ioutil.WriteFile(filename, []byte(context), 0644)
+		So(err, ShouldBeNil)
+
+		Convey("When 读取配置文件", func() {
+			config := viper.New()
+
+			config.AddConfigPath(".")
+			config.SetConfigName("test")
+			config.SetConfigType("json")
+
+			err := config.ReadInConfig()
+			So(err, ShouldBeNil)
+
+			So(config.GetString("host.address"), ShouldEqual, "localhost")
+			So(config.GetInt("datastore.metric.port"), ShouldEqual, 3099)
+		})
+
+		Convey("When 删除文件", func() {
+			os.Remove(filename)
+		})
 	})
 }
-
