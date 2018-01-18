96e4139802cf683bcf52d89bf0c04be4327d1709
hatlonely <hatlonely@gmail.com>
2018-01-18 23:38:28 +0800

    update

--- internal/buildin/chan_test.go
+++ internal/buildin/chan_test.go
@@ -42,6 +42,73 @@ func TestProducerConsumer(t *testing.T) {
 	stop = true
 
 	for i := 0; i < 10; i++ {
-		<- done
+		<-done
+	}
+}
+
+func TestSelect(t *testing.T) {
+	type ProductA struct {
+		name  int
+		value int
+	}
+
+	type ProductB struct {
+		name  int
+		value int
+	}
+
+	productAs := make(chan ProductA, 10)
+	productBs := make(chan ProductB, 10)
+
+	stopA := false
+	stopB := false
+	done := make(chan struct{}, 15)
+
+	producerA := func(name int) {
+		for !stopA {
+			product := ProductA{name: name, value: rand.Int()}
+			productAs <- product
+			t.Logf("produceA %v a product: %#v\n", name, product)
+			time.Sleep(time.Duration(200+rand.Intn(1000)) * time.Millisecond)
+		}
+		done <- struct{}{}
+	}
+	producerB := func(name int) {
+		for !stopB {
+			product := ProductB{name: name, value: rand.Int()}
+			productBs <- product
+			t.Logf("produceB %v a product: %#v\n", name, product)
+			time.Sleep(time.Duration(200+rand.Intn(1000)) * time.Millisecond)
+		}
+		done <- struct{}{}
+	}
+	consumer := func(name int) {
+		ticker := time.Tick(time.Duration(500) * time.Millisecond)
+		for !stopA || !stopB || len(productAs) != 0 || len(productBs) != 0 {
+			select {
+			case product := <-productAs:
+				t.Logf("consume %v a productA: %#v\n", name, product)
+			case product := <-productBs:
+				t.Logf("consume %v a productB: %#v\n", name, product)
+			case <-ticker:
+				// nothing to do just awake from block
+			}
+		}
+		done <- struct{}{}
+	}
+
+	for i := 0; i < 5; i++ {
+		go producerA(i)
+		go producerB(i)
+		go consumer(i)
+	}
+
+	time.Sleep(time.Duration(1) * time.Second)
+	stopA = true
+	time.Sleep(time.Duration(1) * time.Second)
+	stopB = true
+
+	for i := 0; i < 15; i++ {
+		<-done
 	}
 }
