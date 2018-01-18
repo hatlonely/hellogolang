9481af6e831f248846a9e38a210f158a550649a9
hatlonely <hatlonely@gmail.com>
2018-01-18 22:56:08 +0800

    update

--- internal/buildin/chan_test.go
+++ internal/buildin/chan_test.go
@@ -11,21 +11,26 @@ func TestProducerConsumer(t *testing.T) {
 		name  int
 		value int
 	}
+
+	stop := false
+	done := make(chan struct{}, 10)
 	products := make(chan Product, 10)
 	producer := func(name int) {
-		for {
+		for !stop {
 			product := Product{name: name, value: rand.Int()}
 			products <- product
 			t.Logf("produce %v a product: %#v\n", name, product)
 			time.Sleep(time.Duration(200+rand.Intn(1000)) * time.Millisecond)
 		}
+		done <- struct{}{}
 	}
 	consumer := func(name int) {
-		for {
+		for !stop && len(products) != 0 {
 			product := <-products
 			t.Logf("consume %v a product: %#v\n", name, product)
 			time.Sleep(time.Duration(200+rand.Intn(1000)) * time.Millisecond)
 		}
+		done <- struct{}{}
 	}
 
 	for i := 0; i < 5; i++ {
@@ -34,4 +39,9 @@ func TestProducerConsumer(t *testing.T) {
 	}
 
 	time.Sleep(time.Duration(1) * time.Second)
+	stop = true
+
+	for i := 0; i < 10; i++ {
+		<- done
+	}
 }
