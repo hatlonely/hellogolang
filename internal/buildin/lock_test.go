3c38a72065484a2e9858f801b4261aa03ae658ea
hatlonely <hatlonely@gmail.com>
2018-01-19 13:08:22 +0800

    update

--- internal/buildin/lock_test.go
+++ internal/buildin/lock_test.go
@@ -1,1 +1,37 @@
 package buildin
+
+import (
+	"testing"
+	"sync"
+	"strconv"
+)
+
+
+func BenchmarkRWLock(b *testing.B) {
+	cache := make(map[string]string)
+	mutex := sync.RWMutex{}
+	b.RunParallel(func(pb *testing.PB) {
+		count := 0
+		for pb.Next()  {
+			count++
+			str := strconv.Itoa(count)
+
+			mutex.Lock()
+			cache["key" + str] = str
+			mutex.Unlock()
+		}
+	})
+	b.RunParallel(func(pb *testing.PB) {
+		count := 0
+		for pb.Next()  {
+			count++
+			str := strconv.Itoa(count)
+
+			mutex.RLock()
+			_ = cache["key" + str]
+			mutex.RUnlock()
+		}
+	})
+}
+
+
