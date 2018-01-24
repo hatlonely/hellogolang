69de6b1ea2253772bc2ed4218960be86e2fce32a
hatlonely <hatlonely@gmail.com>
2018-01-24 13:19:57 +0800

    update

--- internal/buildin/string_test.go
+++ internal/buildin/string_test.go
@@ -1,1 +1,73 @@
 package buildin
+
+import (
+	"fmt"
+	"testing"
+	"strings"
+	"bytes"
+	"strconv"
+)
+
+func BenchmarkAddStringWithOperator(b *testing.B) {
+	hello := "hello"
+	world := "world"
+	for i := 0; i < b.N; i++ {
+		_ = hello + "=" + world
+	}
+}
+
+func BenchmarkAddStringWithSprintf(b *testing.B) {
+	hello := "hello"
+	world := "world"
+	for i := 0; i < b.N; i++ {
+		_ = fmt.Sprintf("%s=%s", hello, world)
+	}
+}
+
+func BenchmarkAddStringWithJoin(b *testing.B) {
+	hello := "hello"
+	world := "world"
+	for i := 0; i < b.N; i++ {
+		_ = strings.Join([]string{hello, world}, "=")
+	}
+}
+
+func BenchmarkAddStringWithBuffer(b *testing.B) {
+	hello := "hello"
+	world := "world"
+	for i := 0; i < 1000; i++ {
+		var buffer bytes.Buffer
+		buffer.WriteString(hello)
+		buffer.WriteString("=")
+		buffer.WriteString(world)
+		_ = buffer.String()
+	}
+}
+
+func BenchmarkAddStringNumberWithOperator(b *testing.B) {
+	hello := "hello"
+	world := int64(1234567890)
+	for i := 0; i < b.N; i++ {
+		_ = hello + "=" + strconv.FormatInt(world, 10)
+	}
+}
+
+func BenchmarkAddStringNumberWithSprintf(b *testing.B) {
+	hello := "hello"
+	world := int64(1234567890)
+	for i := 0; i < b.N; i++ {
+		_ = fmt.Sprintf("%s=%d", hello, world)
+	}
+}
+
+func BenchmarkAddStringNumberWithBuffer(b *testing.B) {
+	hello := "hello"
+	world := int64(1234567890)
+	for i := 0; i < b.N; i++ {
+		var buffer bytes.Buffer
+		buffer.WriteString(hello)
+		buffer.WriteString("=")
+		buffer.WriteString(strconv.FormatInt(world, 10))
+		_ = buffer.String()
+	}
+}
