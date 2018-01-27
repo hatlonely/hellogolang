a7f70a283d708613f7acf68d3481d85846c8c453
hatlonely <hatlonely@gmail.com>
2018-01-27 21:58:11 +0800

    update

---
+++ internal/json/book.go
@@ -0,0 +1,10 @@
+package json
+
+type Book struct {
+	BookId int     `json:"id"`
+	Title  string  `json:"name"`
+	Author string  `json:"author"`
+	Price  float64 `json:"price,omitempty"` // omitempty 表示忽略控制
+	Hot	   bool    `json:"hot,string"`		// string 表示输出成字符串
+	Weight int     `json:"-"`               // '-' 表示不序列化
+}
