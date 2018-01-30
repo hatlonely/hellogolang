ee600f2955aa84a96a422cd2bb282b4fd4fe6f8e
hatlonely <hatlonely@gmail.com>
2018-01-30 15:40:57 +0800

    update rta_mock

--- cmd/pprof_runtime.go
+++ cmd/pprof_runtime.go
@@ -34,11 +34,11 @@ func main() {
 	go doSomething2()
 	go doSomething3()
 
-	if err := pprof.PPCmd("mem"); err != nil {
+	if err := pprof.PPCmd("cpu 10s"); err != nil {
 		panic(err)
 	}
 
-	if err := pprof.PPCmd("cpu 10s"); err != nil {
+	if err := pprof.PPCmd("mem"); err != nil {
 		panic(err)
 	}
 }
