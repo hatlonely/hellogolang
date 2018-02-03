d5322de3b75de8bbfcd250783dd55683dd0023e8
hatlonely <hatlonely@gmail.com>
2018-02-03 20:03:13 +0800

    update

---
+++ cmd/grpc/echo_client.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"google.golang.org/grpc"
+	"fmt"
+	"hellogolang/api/echo_proto"
+	"context"
+	"os"
+	"strings"
+)
+
+func main() {
+	conn, err := grpc.Dial("127.0.0.1:3000", grpc.WithInsecure())
+	if err != nil {
+		fmt.Errorf("dial failed. err: [%v]\n", err)
+		return
+	}
+
+	client := echo.NewEchoClient(conn)
+	res, err := client.Echo(context.Background(), &echo.EchoReq{
+		Msg: strings.Join(os.Args[1:], " "),
+	})
+
+	if err != nil {
+		fmt.Errorf("client echo failed. err: [%v]", err)
+	} else {
+		fmt.Printf("message from server: %v", res.GetMsg())
+	}
+}
---
+++ cmd/grpc/echo_server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"api/echo_proto"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"net"
+	"fmt"
+)
+
+type EchoServerImp struct {
+
+}
+
+func (e *EchoServerImp) Echo(ctx context.Context, req *echo.EchoReq) (*echo.EchoRes, error) {
+	fmt.Printf("message from client: %v\n", req.GetMsg())
+
+	res := &echo.EchoRes{
+		Msg: req.GetMsg(),
+	}
+
+	return res, nil
+}
+
+func main() {
+	server := grpc.NewServer()
+
+	address, err := net.Listen("tcp", ":3000")
+	if err != nil {
+		panic(err)
+	}
+
+	echo.RegisterEchoServer(server, &EchoServerImp{})
+
+	if err := server.Serve(address); err != nil {
+		panic(err)
+	}
+}
