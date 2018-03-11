9a18283c1858ee15293d4310296e224a54fe0099
hatlonely <hatlonely@gmail.com>
2018-03-12 00:48:59 +0800

    update

---
+++ internal/buildin/struct_test.go
@@ -0,0 +1,66 @@
+package buildin
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+	"unsafe"
+)
+
+func TestStructSize(t *testing.T) {
+	Convey("基本数据类型大小检测", t, func() {
+		So(unsafe.Sizeof(int8(0)), ShouldEqual, 1)
+		So(unsafe.Sizeof(int16(0)), ShouldEqual, 2)
+		So(unsafe.Sizeof(int32(0)), ShouldEqual, 4)
+		So(unsafe.Sizeof(int64(0)), ShouldEqual, 8)
+		So(unsafe.Sizeof(float32(0)), ShouldEqual, 4)
+		So(unsafe.Sizeof(float64(0)), ShouldEqual, 8)
+		So(unsafe.Sizeof(""), ShouldEqual, 16)
+		So(unsafe.Sizeof([]int{}), ShouldEqual, 24)
+		So(unsafe.Sizeof([]int{1, 2, 3}), ShouldEqual, 24)
+		So(unsafe.Sizeof(map[string]string{}), ShouldEqual, 8)
+		So(unsafe.Sizeof(map[string]string{"1": "one", "2": "two"}), ShouldEqual, 8)
+		So(unsafe.Sizeof(struct{}{}), ShouldEqual, 0)
+	})
+
+	Convey("自定义类型大小检测", t, func() {
+		So(unsafe.Sizeof(struct {
+			i8 int8
+		}{}), ShouldEqual, 1)
+
+		So(unsafe.Sizeof(struct {
+			i8  int8
+			i32 int32
+			i16 int16
+		}{}), ShouldEqual, 12)
+
+		So(unsafe.Sizeof(struct {
+			i8  int8
+			i16 int16
+			i32 int32
+		}{}), ShouldEqual, 8)
+
+		So(unsafe.Sizeof(struct {
+			i8  int8
+			i32 int32
+			i16 int16
+			i64 int64
+		}{}), ShouldEqual, 24)
+
+		So(unsafe.Sizeof(struct {
+			i8  int8
+			i16 int16
+			i32 int32
+			i64 int64
+		}{}), ShouldEqual, 16)
+
+		type I8 int8
+		type I16 int16
+		type I32 int32
+
+		So(unsafe.Sizeof(struct {
+			i8  I8
+			i16 I16
+			i32 I32
+		}{}), ShouldEqual, 8)
+	})
+}
