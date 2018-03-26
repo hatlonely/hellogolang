d176edffaf4a72634511a62c8b3426fe23784b17
hatlonely <hatlonely@gmail.com>
2018-03-26 18:36:25 +0800

    update

--- internal/logrus/logrus_test.go
+++ internal/logrus/logrus_test.go
@@ -9,6 +9,8 @@ import (
 
 	"github.com/sirupsen/logrus"
 	. "github.com/smartystreets/goconvey/convey"
+	"github.com/sohlich/elogrus"
+	"gopkg.in/olivere/elastic.v5"
 )
 
 type Person struct {
@@ -67,5 +69,36 @@ func TestLogrus(t *testing.T) {
 			_, file, line, _ := runtime.Caller(0)
 			log.WithFields(logrus.Fields{"file": file, "line": line}).Infof("hello world")
 		})
+
+		Convey("logrus entry 赋值", func() {
+			log := logrus.New()
+			log.Out = os.Stdout
+			log.Formatter = &logrus.JSONFormatter{}
+			mylog := log.WithFields(logrus.Fields{"key1": "val1", "key2": 2})
+			mylog.WithFields(logrus.Fields{"animal": "walrus", "size": 10}).Infof("A group of walrus emerges from the ocean")
+			So(1, ShouldEqual, 2)
+		})
+	})
+}
+
+func TestLogrusHook(t *testing.T) {
+	Convey("logrus hook", t, func() {
+		log := logrus.New()
+		client, err := elastic.NewClient(elastic.SetURL("http://localhost:9200"))
+		So(err, ShouldEqual, nil)
+		// _index: testlog, 日志级别: Warn
+		hook, err := elogrus.NewAsyncElasticHook(client, "localhost", logrus.WarnLevel, "testlog")
+		So(err, ShouldBeNil)
+		log.AddHook(hook)
+		log.WithFields(logrus.Fields{
+			"name": "playjokes",
+			"age":  15,
+		}).Warn("Hello world!")
+
+		// 这个不会写入
+		log.WithFields(logrus.Fields{
+			"name": "hatlonely",
+			"age":  100,
+		}).Info("Hello world!")
 	})
 }
