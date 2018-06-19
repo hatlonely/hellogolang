a9736c2a6eb722f522c77a39f1a7dd1521d8c585
hatlonely <hatlonely@gmail.com>
2018-06-19 12:10:29 +0800

    update

--- internal/buildin/object_oriented_test.go
+++ internal/buildin/object_oriented_test.go
@@ -7,6 +7,8 @@ import (
 
 type Sayer interface {
 	Say()
+	SayHello()
+	SayWorld()
 }
 
 type Animal struct {
@@ -14,6 +16,20 @@ type Animal struct {
 
 func (a *Animal) Say() {
 	fmt.Println("not implement")
+}
+
+func (a *Animal) SayHello() {
+	a.Say()
+	fmt.Println("hello")
+}
+
+func SayWorld(sayer Sayer) {
+	sayer.Say()
+	fmt.Println("world")
+}
+
+func (a *Animal) SayWorld() {
+	SayWorld(a)
 }
 
 type Dog struct {
@@ -24,12 +40,20 @@ func (d *Dog) Say() {
 	fmt.Println("wang wang wang")
 }
 
+func (d *Dog) SayWorld() {
+	SayWorld(d)
+}
+
 type Cat struct {
 	Animal
 }
 
-func (d *Cat) Say() {
+func (c *Cat) Say() {
 	fmt.Println("miao miao miao")
+}
+
+func (c *Cat) SayWorld() {
+	SayWorld(c)
 }
 
 type Mouse struct {
@@ -39,14 +63,25 @@ type Mouse struct {
 func TestObjectOriented(t *testing.T) {
 	var sayer Sayer
 
+	// 有继承，有限的多态特性，父类的实现中无法调用子类的方法
 	sayer = &Dog{}
-	sayer.Say()
+	sayer.Say()      // wang wang wang
+	sayer.SayHello() // not implement hello
+	sayer.SayWorld() // wang wang wang world
+
+	fmt.Println()
 
 	sayer = &Cat{}
 	sayer.Say()
+	sayer.SayHello()
+	sayer.SayWorld()
+
+	fmt.Println()
 
 	sayer = &Mouse{}
 	sayer.Say()
+	sayer.SayHello()
+	sayer.SayWorld()
 
-	t.Error(1)
+	fmt.Println()
 }
