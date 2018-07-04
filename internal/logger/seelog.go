649ff0eab31a8a4f9c7a947423ce5e4e16cb960b
hatlonely <hatlonely@gmail.com>
2018-07-04 17:02:12 +0800

    update

--- internal/logger/seelog.go
+++ internal/logger/seelog.go
@@ -4,9 +4,11 @@ import "github.com/cihub/seelog"
 
 func NewSeeLog(filename string) *SeeLog {
 	testConfig := `<seelog>
-    <outputs formatid="access">
-        <rollingfile type="date" filename="` + filename + `" datepattern="2006-01-02-15" maxrolls="240"/>
-    </outputs>
+	<outputs formatid="access">
+		<buffered formatid="access" size="10000" flushperiod="100">
+        	<rollingfile type="date" filename="` + filename + `" datepattern="2006-01-02-15" maxrolls="240"/>
+		</buffered>
+	</outputs>
     <formats>
         <format id="access" format="%Msg%n"/>
     </formats>
