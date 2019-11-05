6dc78d3e08ddb4334738d10a460007766d115332
hatlonely <hatlonely@gmail.com>
2019-11-05 16:44:36 +0800

    update

--- internal/buildin/reflect_test.go
+++ internal/buildin/reflect_test.go
@@ -1,7 +1,6 @@
 package buildin
 
 import (
-	"fmt"
 	"reflect"
 	"testing"
 
@@ -31,50 +30,38 @@ func (a A) Sum(vi ...int) int {
 	return sum + a.F1
 }
 
-func TestReflect(t *testing.T) {
+func TestReflectType(t *testing.T) {
 	Convey("遍历类", t, func() {
 		t := reflect.TypeOf(A{})
-		for i := 0; i < t.NumField(); i++ {
-			field := t.Field(i)
-			fmt.Println(field.Name)
-			fmt.Println(field.Type)
-			fmt.Println(field.Tag)
-		}
+		So(t.NumField(), ShouldEqual, 2)
+		So(t.Field(0).Name, ShouldEqual, "F1")
+		So(t.Field(0).Type.Kind(), ShouldEqual, reflect.Int)
+		So(t.Field(0).Tag, ShouldEqual, `json:"f1"`)
+		So(t.Field(1).Name, ShouldEqual, "F2")
+		So(t.Field(1).Type.Kind(), ShouldEqual, reflect.String)
+		So(t.Field(1).Tag, ShouldEqual, `json:"f2"`)
 	})
 
 	Convey("获取 tag", t, func() {
 		So(reflect.TypeOf(A{}).Field(0).Tag.Get("json"), ShouldEqual, "f1")
 		So(reflect.TypeOf(A{}).Field(1).Tag.Get("json"), ShouldEqual, "f2")
 	})
+}
 
+func TestReflectValue(t *testing.T) {
 	Convey("遍历值", t, func() {
 		a := A{
 			F1: 10,
 			F2: "hatlonely",
 		}
 		v := reflect.ValueOf(a)
-		for i := 0; i < v.NumField(); i++ {
-			val := v.Field(i)
-			fmt.Println(val.Type().Kind())
-			switch val.Type().Kind() {
-			case reflect.Int:
-				fmt.Println(val.Int())
-			case reflect.String:
-				fmt.Println(val.String())
-			}
-		}
-	})
-
-	Convey("根据字段名获取值", t, func() {
-		a := A{
-			F1: 10,
-			F2: "hatlonely",
-		}
-
-		So(reflect.ValueOf(a).FieldByName("F1").Type().Kind(), ShouldEqual, reflect.Int)
-		So(reflect.ValueOf(a).FieldByName("F2").Type().Kind(), ShouldEqual, reflect.String)
-		So(reflect.ValueOf(a).FieldByName("F1").Int(), ShouldEqual, 10)
-		So(reflect.ValueOf(a).FieldByName("F2").String(), ShouldEqual, "hatlonely")
+		So(v.NumField(), ShouldEqual, 2)
+		So(v.Field(0).Int(), ShouldEqual, 10)
+		So(v.Field(1).String(), ShouldEqual, "hatlonely")
+		So(v.Field(0).Type().Kind(), ShouldEqual, reflect.Int)
+		So(v.Field(1).Type().Kind(), ShouldEqual, reflect.String)
+		So(v.FieldByName("F1").Int(), ShouldEqual, 10)
+		So(v.FieldByName("F2").Interface().(string), ShouldEqual, "hatlonely")
 	})
 }
 
