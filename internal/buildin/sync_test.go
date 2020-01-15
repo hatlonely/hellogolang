3100f1f911695bab7e6b1e738581018390970d0b
hatlonely <hatlonely@gmail.com>
2020-01-15 22:39:52 +0800

    update

--- internal/buildin/sync_test.go
+++ internal/buildin/sync_test.go
@@ -2,10 +2,12 @@ package buildin
 
 import (
 	"fmt"
+	"math/rand"
 	"strconv"
 	"sync"
 	"sync/atomic"
 	"testing"
+	"time"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
@@ -169,4 +171,94 @@ func TestOnce(t *testing.T) {
 			So(count, ShouldEqual, 1)
 		}
 	})
+}
+
+func TestCond(t *testing.T) {
+	Convey("test cond", t, func() {
+		var wg sync.WaitGroup
+		deadline := time.Now().Add(1 * time.Second)
+		q := NewBlockingQueue(4)
+		for i := 0; i < 10; i++ {
+			wg.Add(1)
+			go func(i int) {
+				rd := rand.New(rand.NewSource(time.Now().UnixNano()))
+				for time.Now().Before(deadline) {
+					k := rd.Int() % 100
+					q.put(k)
+					fmt.Println(i, "put", k)
+				}
+				wg.Done()
+			}(i)
+		}
+		for i := 0; i < 10; i++ {
+			wg.Add(1)
+			go func(i int) {
+				for time.Now().Before(deadline) {
+					fmt.Println(i, "take", q.take())
+				}
+				wg.Done()
+			}(i)
+		}
+		wg.Wait()
+	})
+}
+
+type BlockingQueue struct {
+	mutex    *sync.Mutex
+	notFull  *sync.Cond
+	notEmpty *sync.Cond
+	start    int
+	end      int
+	capacity int
+	vector   []interface{}
+}
+
+func NewBlockingQueue(capacity int) *BlockingQueue {
+	var mutex sync.Mutex
+	return &BlockingQueue{
+		mutex:    &mutex,
+		notFull:  sync.NewCond(&mutex),
+		notEmpty: sync.NewCond(&mutex),
+		start:    0,
+		end:      0,
+		capacity: capacity,
+		vector:   make([]interface{}, capacity+1),
+	}
+}
+
+func (q *BlockingQueue) isEmpty() bool {
+	return q.start == q.end
+}
+
+func (q *BlockingQueue) Size() int {
+	return (q.end - q.start + q.capacity + 1) % (q.capacity + 1)
+}
+
+func (q *BlockingQueue) isFull() bool {
+	return q.Size() == q.capacity
+}
+
+func (q *BlockingQueue) put(v interface{}) {
+	q.mutex.Lock()
+	defer q.mutex.Unlock()
+	for q.isFull() {
+		q.notFull.Wait()
+	}
+	q.vector[q.end] = v
+	q.end++
+	q.end %= q.capacity + 1
+	q.notEmpty.Signal()
+}
+
+func (q *BlockingQueue) take() interface{} {
+	q.mutex.Lock()
+	defer q.mutex.Unlock()
+	for q.isEmpty() {
+		q.notEmpty.Wait()
+	}
+	res := q.vector[q.start]
+	q.start++
+	q.start %= q.capacity + 1
+	q.notFull.Signal()
+	return res
 }
