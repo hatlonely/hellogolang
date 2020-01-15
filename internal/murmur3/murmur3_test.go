d5e9f5276dccfe291f19011aade9ceaecd119eeb
hatlonely <hatlonely@gmail.com>
2020-01-16 00:16:59 +0800

    update

--- internal/murmur3/murmur3_test.go
+++ internal/murmur3/murmur3_test.go
@@ -2,18 +2,18 @@ package murmur3
 
 import (
 	"testing"
+
 	. "github.com/smartystreets/goconvey/convey"
 	"github.com/spaolacci/murmur3"
 )
 
 func TestMurmur3(t *testing.T) {
-	Convey("Given 一个字符串", t, func() {
-		message := "hello golang"
-		Convey("When 使用murmur3哈希", func() {
-			val := murmur3.Sum64([]byte(message))
-			Convey("Then hash的结果正确", func() {
-				So(val, ShouldEqual, uint64(12958155624392757344))
-			})
-		})
+	Convey("test murmur3", t, func() {
+		h := murmur3.New64()
+		So(h.Size(), ShouldEqual, 16)
+		_, _ = h.Write([]byte("hello world"))
+		So(h.Sum64(), ShouldEqual, uint64(5998619086395760910))
+
+		So(murmur3.Sum64([]byte("hello world")), ShouldEqual, uint64(5998619086395760910))
 	})
 }
