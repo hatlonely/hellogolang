626732dbcf02b1829ef835c66057b307c2cf24af
hatlonely <hatlonely@gmail.com>
2020-01-16 00:42:45 +0800

    update

--- internal/viper/viper_test.go
+++ internal/viper/viper_test.go
@@ -10,9 +10,10 @@ import (
 )
 
 func TestViper(t *testing.T) {
-	Convey("Given 给一个配置文件", t, func() {
+	Convey("test viper", t, func() {
 		filename := "test.json"
-		context := `{
+
+		So(ioutil.WriteFile(filename, []byte(`{
     "host": {
         "address": "localhost",
         "port": 5799
@@ -27,28 +28,20 @@ func TestViper(t *testing.T) {
             "port": 2112
         }
     }
-}`
+}`), 0644), ShouldBeNil)
 
-		err := ioutil.WriteFile(filename, []byte(context), 0644)
-		So(err, ShouldBeNil)
+		config := viper.New()
+		config.AddConfigPath(".")
+		config.SetConfigName("test")
+		config.SetConfigType("json")
 
-		Convey("When 读取配置文件", func() {
-			config := viper.New()
+		So(config.ReadInConfig(), ShouldBeNil)
 
-			config.AddConfigPath(".")
-			config.SetConfigName("test")
-			config.SetConfigType("json")
+		So(config.GetString("host.address"), ShouldEqual, "localhost")
+		So(config.GetInt("datastore.metric.port"), ShouldEqual, 3099)
+		So(config.Sub("datastore").GetInt("metric.port"), ShouldEqual, 3099)
+		So(config.Sub("abc"), ShouldBeNil)
 
-			err := config.ReadInConfig()
-			So(err, ShouldBeNil)
-
-			So(config.GetString("host.address"), ShouldEqual, "localhost")
-			So(config.GetInt("datastore.metric.port"), ShouldEqual, 3099)
-			So(config.Sub("abc"), ShouldBeNil)
-		})
-
-		Convey("Finally 删除文件", func() {
-			os.Remove(filename)
-		})
+		So(os.Remove(filename), ShouldBeNil)
 	})
 }
