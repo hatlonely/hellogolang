e656163d6e7ecf480888c52325ec2848eda5447b
hatlonely <hatlonely@gmail.com>
2020-01-16 00:28:06 +0800

    update

--- internal/seelog/seelog_test.go
+++ internal/seelog/seelog_test.go
@@ -1,17 +1,17 @@
 package seelog
 
 import (
-	"testing"
-	"github.com/cihub/seelog"
-	. "github.com/smartystreets/goconvey/convey"
 	"io/ioutil"
 	"os"
+	"testing"
+
+	"github.com/cihub/seelog"
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestSeelog(t *testing.T) {
-	Convey("Given 一个配置文件", t, func() {
-		filename := "sample.xml"
-		context := `<seelog>
+	Convey("test seelog", t, func() {
+		config := `<seelog>
     <outputs>
         <filter levels="debug, trace, info, warn, error, critical">
             <buffered formatid="runtime" size="10000" flushperiod="100">
@@ -23,43 +23,32 @@ func TestSeelog(t *testing.T) {
         <format id="runtime" format="%Date %Time [%Level] [%RelFile:%Line] [%Func] %Msg%n"/>
     </formats>
 </seelog>`
-		err := ioutil.WriteFile(filename, []byte(context), 0644)
-		So(err, ShouldBeNil)
 
+		var err error
 		var woklog seelog.LoggerInterface
-		Convey("When 从配置文件中读取", func() {
-			woklog, err = seelog.LoggerFromConfigAsFile(filename)
+		Convey("load from file", func() {
+			So(ioutil.WriteFile("sample.xml", []byte(config), 0644), ShouldBeNil)
+			woklog, err = seelog.LoggerFromConfigAsFile("sample.xml")
 			So(err, ShouldBeNil)
 			So(woklog, ShouldNotBeNil)
-
-			Convey("Then 写日志", func() {
-				woklog.Infof("Hello seelog!")
-			})
+			woklog.Infof("Hello seelog!")
+			woklog.Close()
+			So(os.Remove("sample.xml"), ShouldBeNil)
+			So(os.RemoveAll("log"), ShouldBeNil)
 		})
 
-		Convey("When 从buffer中读取", func() {
-			woklog, err = seelog.LoggerFromConfigAsBytes([]byte(context))
+		Convey("load from byte", func() {
+			woklog, err = seelog.LoggerFromConfigAsBytes([]byte(config))
 			So(err, ShouldBeNil)
 			So(woklog, ShouldNotBeNil)
-
-			Convey("Then 写日志", func() {
-				woklog.Infof("Hello seelog!")
-			})
+			woklog.Infof("Hello seelog!")
 		})
 
-		Convey("When 替换默认logger", func() {
-			woklog, err = seelog.LoggerFromConfigAsBytes([]byte(context))
+		Convey("replace logger", func() {
+			woklog, err = seelog.LoggerFromConfigAsBytes([]byte(config))
 			seelog.ReplaceLogger(woklog)
 			So(seelog.Current, ShouldEqual, woklog)
-
-			Convey("Then 写日志", func() {
-				seelog.Infof("Hello seelog!")
-			})
-		})
-
-		Convey("Finally 删除文件", func() {
-			So(os.Remove(filename), ShouldBeNil)
-			So(os.RemoveAll("log"), ShouldBeNil)
+			seelog.Infof("Hello seelog!")
 		})
 	})
 }
